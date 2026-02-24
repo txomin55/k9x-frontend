e91421498e4bf1b6b3083523594aabb5b6177059
txomin <txomin.sirera@clarity.ai>
2026-02-24 12:54:56 +0100

    try ci

--- ci-scripts/unit-tests.sh
+++ ci-scripts/unit-tests.sh
@@ -5,6 +5,6 @@ mkdir -p .reports/sonar
 pnpm run test:unit:coverage
 
 pnpm run unit:coverage-merge
-pnpm run unit:merge
+pnpm run unit:junit-merge
 cp -r ui/app/.reports/sonar .reports/sonar/app
 cp -r ui/library/.reports/sonar .reports/sonar/library
