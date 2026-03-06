8749a30b20b53377176db4e390c990247649b692
txomin <txomin.sirera@clarity.ai>
2026-03-06 22:27:31 +0100

    remove login service

--- ui/app/src/App.jsx
+++ ui/app/src/App.jsx
@@ -1,41 +1,18 @@
 import { useAuthentication } from "@/providers/authentication/AuthenticationProvider";
 import { useI18n } from "@/providers/i18n/I18nProvider.jsx";
-import { useApi } from "@/providers/api/ApiProvider.jsx";
 import { Link, useNavigate } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import doLogin from "@/services/do_login/doLogin.js";
-import fetchUserData from "@/services/fetch_user_data/fetchUserData.js";
+import React, { useState } from "react";
 import { ROUTES } from "@/routes/index.js";
 import CoreButton from "@lib/components/atoms/button/CoreButton.jsx";
 
 function App({ children }) {
   const { t, locale, setLocale, locales } = useI18n();
 
-  const getApi = useApi();
-  const { login, getUserData } = getApi();
-  const { setUser, user } = useAuthentication();
+  const { user } = useAuthentication();
 
   const navigate = useNavigate();
 
   const [isDark, setIsDark] = useState(false);
-
-  useEffect(() => {
-    if (window.location.search !== "") {
-      const urlParams = new URLSearchParams(window.location.search);
-      const code = urlParams.get("code");
-      if (code !== "") {
-        doLogin(
-          () => login(null, { code }),
-          () => {
-            fetchUserData(getUserData, (d) => {
-              setUser(d);
-              navigate(ROUTES.HOME);
-            });
-          },
-        );
-      }
-    }
-  }, [login, getUserData, setUser, navigate]);
 
   const toggleMode = () => {
     if (isDark) {
