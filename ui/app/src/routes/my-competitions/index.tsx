9e9a69e802c8c821d4a4d8bd81e67fc4cccd8083
txomin <txomin.sirera@clarity.ai>
2026-03-25 14:48:39 +0100

    move into file-route mode

--- ui/app/src/routes/my-competitions/index.tsx
+++ ui/app/src/routes/my-competitions/index.tsx
@@ -1,32 +1,9 @@
-import { useCompetitions } from "@/services/fetch_competitions/fetchCompetitions";
-import { For, Suspense } from "solid-js";
-import CompetitionCard from "@/components/routes/my-competitions/competition-card/CompetitionCard";
+import { createFileRoute, Navigate } from "@tanstack/solid-router";
 
-export default function MyCompetitionsRoute() {
-  const fetchedCompetitions = useCompetitions({
-    staleTime: 30 * 1000,
-    refetchOnMount: false,
-    gcTime: 5 * 60 * 1000,
-  });
+export const Route = createFileRoute("/my-competitions/")({
+  component: MyCompetitionsRedirectPage,
+});
 
-  return (
-    <div class="my-competitionss">
-      <h1>--My competitions</h1>
-      <Suspense fallback={<span>--Loading Competitions</span>}>
-        <For each={fetchedCompetitions.data}>
-          {(competition) => (
-            <CompetitionCard
-              id={competition.id}
-              status={competition.status}
-              name={competition.name}
-              description={competition.description}
-              country={competition.country}
-              stages={competition.stages}
-              address={competition?.location?.address}
-            />
-          )}
-        </For>
-      </Suspense>
-    </div>
-  );
+function MyCompetitionsRedirectPage() {
+  return <Navigate to="/my-competitions/list" replace />;
 }
---
+++ ui/app/src/routes/my-competitions/list/index.tsx
@@ -0,0 +1,37 @@
+import { createFileRoute } from "@tanstack/solid-router";
+import { For, Suspense } from "solid-js";
+import CompetitionCard from "@/components/routes/my-competitions/competition-card/CompetitionCard";
+import { useCompetitions } from "@/services/fetch_competitions/fetchCompetitions";
+
+export const Route = createFileRoute("/my-competitions/list/")({
+  component: MyCompetitionsIndexPage,
+});
+
+function MyCompetitionsIndexPage() {
+  const fetchedCompetitions = useCompetitions({
+    staleTime: 30 * 1000,
+    refetchOnMount: false,
+    gcTime: 5 * 60 * 1000,
+  });
+
+  return (
+    <div class="my-competitionss">
+      <h1>--My competitions</h1>
+      <Suspense fallback={<span>--Loading Competitions</span>}>
+        <For each={fetchedCompetitions.data}>
+          {(competition) => (
+            <CompetitionCard
+              id={competition.id}
+              status={competition.status}
+              name={competition.name}
+              description={competition.description}
+              country={competition.country}
+              stages={competition.stages}
+              address={competition?.location?.address}
+            />
+          )}
+        </For>
+      </Suspense>
+    </div>
+  );
+}
