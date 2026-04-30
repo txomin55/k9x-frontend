83c1ceee6bcbb5e2448401251ba338c39c0a82f9
txomin <txomin.sirera@clarity.ai>
2026-04-30 11:18:30 +0200

    organizer form inclusion

---
+++ ui/app/src/components/global/app-shell/layout/navigation/OrganizerForm.tsx
@@ -0,0 +1,28 @@
+import AtomTextArea from "@lib/components/atoms/text-area/AtomTextArea";
+import { createSignal } from "solid-js";
+import AtomButton from "@lib/components/atoms/button/AtomButton";
+import { useAuthUser } from "@/stores/auth";
+import postGoogleForm from "@/utils/google-forms/postGoogleForm";
+import AtomInput from "@lib/components/atoms/input/AtomInput";
+
+export default function OrganizerForm() {
+  const user = useAuthUser();
+
+  const [description, setDescription] = createSignal("");
+  const [organizerName, setOrganizerName] = createSignal("");
+
+  const sendOrganizerForm = () =>
+    postGoogleForm("1FAIpQLScUMXJO8wACFCg4qpcAP5IsfWG5BjeJ5MvhomrgVs-Fh9czUA", {
+      "entry.1603237692": organizerName(),
+      "entry.1931094736": user()?.email,
+      "entry.897265685": description(),
+    });
+
+  return (
+    <div class="organizer-form">
+      <AtomInput value={organizerName()} onChange={setOrganizerName} />
+      <AtomTextArea value={description()} onChange={setDescription} />
+      <AtomButton onClick={sendOrganizerForm}>--Send</AtomButton>
+    </div>
+  );
+}
