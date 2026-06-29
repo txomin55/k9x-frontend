6b821aa271a03dd8280888effed7f34886020302
txomin <txomin.sirera@clarity.ai>
2026-06-29 11:35:23 +0200

    add logo

--- ui/app/src/entry-server.tsx
+++ ui/app/src/entry-server.tsx
@@ -10,6 +10,12 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href={resolveAppPath("/favicon.svg")} />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@900&display=swap"
+            rel="stylesheet"
+          />
           <title>K9X</title>
           {assets}
         </head>
