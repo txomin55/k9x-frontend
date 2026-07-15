89b0c35c116e9de4331c49b69ee89cc2f0aa7107
txomin <txomin.sirera@clarity.ai>
2026-07-15 20:24:42 +0200

    - update rank

--- ui/app/src/components/common/rank-badge/RankBadge.tsx
+++ ui/app/src/components/common/rank-badge/RankBadge.tsx
@@ -1,14 +1,25 @@
 import { Show } from "solid-js";
 import AtomBadge from "@lib/components/atoms/badge/AtomBadge";
+import "./styles.css";
 
 export interface RankBadgeProps {
   rank?: string;
 }
 
 export default function RankBadge(props: RankBadgeProps) {
+  const letter = () => props.rank?.replace("+", "");
+  const hasPlus = () => Boolean(props.rank?.includes("+"));
+
   return (
     <Show when={props.rank}>
-      <AtomBadge textValue={props.rank}>{props.rank}</AtomBadge>
+      <AtomBadge textValue={props.rank}>
+        <span class="rank-badge__content">
+          {letter()}
+          <Show when={hasPlus()}>
+            <span class="rank-badge__plus">+</span>
+          </Show>
+        </span>
+      </AtomBadge>
     </Show>
   );
 }
