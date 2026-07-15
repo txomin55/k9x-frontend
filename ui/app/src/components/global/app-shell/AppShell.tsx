fa45ddd72509a1fa90b4658f37e47fc4a7cddedc
txomin <txomin.sirera@clarity.ai>
2026-07-15 23:20:19 +0200

    - no --loading fix

--- ui/app/src/components/global/app-shell/AppShell.tsx
+++ ui/app/src/components/global/app-shell/AppShell.tsx
@@ -79,7 +79,7 @@ export default function AppShell() {
   return (
     <MetaProvider>
       <Link rel="manifest" href={resolveAppPath("/manifest.webmanifest")} />
-      <Show when={i18n.ready()}}>
+      <Show when={i18n.ready()}>
         <NotificationGuard>
           <AppLayout>
             <div class="app-shell">
