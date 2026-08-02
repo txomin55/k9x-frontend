75d3567d60325450307048bd2d7fdb0723dc38fe
txomin <txomin.sirera@clarity.ai>
2026-08-02 22:10:13 +0200

    more info walkthroughs

--- ui/app/src/routes/my/competitions/route.tsx
+++ ui/app/src/routes/my/competitions/route.tsx
@@ -1,5 +1,9 @@
 import { createFileRoute, Outlet } from "@tanstack/solid-router";
 import BreadcrumbInfoSlides from "@/components/common/breadcrumb-info/BreadcrumbInfoSlides";
+import competitionsCard from "@/assets/breadcrumb-info/competitions-card.webp";
+import competitionsViewModes from "@/assets/breadcrumb-info/competitions-view-modes.webp";
+import competitionsAdd from "@/assets/breadcrumb-info/competitions-add.webp";
+import competitionsStatuses from "@/assets/breadcrumb-info/competitions-statuses.webp";
 
 export const Route = createFileRoute("/my/competitions")({
   component: MyCompetitionsLayoutPage,
@@ -14,5 +18,35 @@ function MyCompetitionsLayoutPage() {
 }
 
 function MyCompetitionsBreadcrumbInfo() {
-  return <BreadcrumbInfoSlides slides={[[{ keys: ["MY.COMPETITIONS.BREADCRUMB_INFO"] }]]} />;
+  return (
+    <BreadcrumbInfoSlides
+      slides={[
+        [{ keys: ["MY.COMPETITIONS.BREADCRUMB_INFO"] }],
+        [
+          {
+            keys: ["MY.COMPETITIONS.BREADCRUMB_INFO_2"],
+            image: competitionsCard,
+          },
+        ],
+        [
+          {
+            keys: ["MY.COMPETITIONS.BREADCRUMB_INFO_3"],
+            image: competitionsViewModes,
+          },
+        ],
+        [
+          {
+            keys: ["MY.COMPETITIONS.BREADCRUMB_INFO_4"],
+            image: competitionsStatuses,
+          },
+        ],
+        [
+          {
+            keys: ["MY.COMPETITIONS.BREADCRUMB_INFO_5"],
+            image: competitionsAdd,
+          },
+        ],
+      ]}
+    />
+  );
 }
